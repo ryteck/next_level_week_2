bd72a9c8dbad786caab820b05c2072e0a62975c1
ryteck <rya_gm@hotmail.com>
2020-08-08 12:42:43 -0300

    dia-4

---
+++ mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { BorderlessButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+import styles from './styles'
+import backIcon from '../../assets/images/icons/back.png'
+import logoImg from '../../assets/images/logo.png'
+
+interface PageHeaderPropsInterface {
+    title: string;
+}
+
+const PageHeader: React.FC<PageHeaderPropsInterface> = ({ title }) => {
+    const { navigate } = useNavigation()
+
+    function handleGoBack() {
+        navigate('landing')
+    }
+
+    return (
+        <View style={styles.container} >
+            <View style={styles.topBar}>
+                <BorderlessButton onPress={handleGoBack}>
+                    <Image source={backIcon} resizeMode="contain" />
+                </BorderlessButton>
+
+                <Image source={logoImg} resizeMode="contain" />
+            </View>
+
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    )
+}
+
+export default PageHeader
